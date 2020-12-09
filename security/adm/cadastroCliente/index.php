ebdcf559d328755e48cc58ceba48e013b1f4d53a
Flavio Henrique <flaviovhtorres@gmail.com>
2020-12-09 01:00:42 -0300

    fixing bugs and responsiveness - home, services

--- security/adm/cadastroCliente/index.php
+++ security/adm/cadastroCliente/index.php
@@ -1,4 +1,8 @@
 <?php
+header("Location: ../home/");
+?>
+
+<?php
 include_once('../../utils/sentinel.php');
 ?>
 
@@ -6,146 +10,150 @@ include_once('../../utils/sentinel.php');
 <html lang="pt-BR">
 
 <head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Bigodudu</title>
-	<!-- fonts -->
-	<link href="https://fonts.googleapis.com/css2?family=Ovo&family=Poppins:wght@500&family=Ubuntu&display=swap" rel="stylesheet" />
-	<script src="https://kit.fontawesome.com/326a212606.js" crossorigin="anonymous"></script>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bigodudu</title>
+  <!-- fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Ovo&family=Poppins:wght@500&family=Ubuntu&display=swap"
+    rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/326a212606.js" crossorigin="anonymous"></script>
 
-	<!-- styles -->
-	<link rel="stylesheet" href="../../../src/styles/reset.css" />
-	<link rel="stylesheet" href="../../../src/styles/default.css" />
-	<link rel="stylesheet" href="../../../src/components/modal/animate.min.css" />
-	<link rel="stylesheet" href="../../../src/components/modal/styles.css" />
-	<link rel="stylesheet" href="../../../src/components/menu/style.css" />
-	<link rel="stylesheet" href="./src/styles.css" />
+  <!-- styles -->
+  <link rel="stylesheet" href="../../../src/styles/reset.css" />
+  <link rel="stylesheet" href="../../../src/styles/default.css" />
+  <link rel="stylesheet" href="../../../src/components/modal/animate.min.css" />
+  <link rel="stylesheet" href="../../../src/components/modal/styles.css" />
+  <link rel="stylesheet" href="../../../src/components/menu/style.css" />
+  <link rel="stylesheet" href="./src/styles.css" />
 
 
-	<!-- scripts -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- scripts -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 </head>
 
 <body>
-	<a id="demo01" href="#animatedModal"></a>
+  <a id="demo01" href="#animatedModal"></a>
 
-	<div id="animatedModal">
-		<!--THIS IS IMPORTANT! to close the modal, the class name has to match the name given on the ID -->
-		<div id="btn-close-modal" class="close-animatedModal">
-			<label id="fechar-modal"><i class="fas fa-times-circle"></i></label>
-		</div>
+  <div id="animatedModal">
+    <!--THIS IS IMPORTANT! to close the modal, the class name has to match the name given on the ID -->
+    <div id="btn-close-modal" class="close-animatedModal">
+      <label id="fechar-modal"><i class="fas fa-times-circle"></i></label>
+    </div>
 
-		<div id="modal" class="modal-content">
-			<!--Your modal content goes here-->
-		</div>
-	</div>
+    <div id="modal" class="modal-content">
+      <!--Your modal content goes here-->
+    </div>
+  </div>
 
-	<div class="splash-screen"></div>
-	<div class="page">
-		<header>
-			<nav>
-				<div class="title-page">
-					<img src="./../../../src/assets/bigode.svg" alt="" />
-				</div>
-				<ul>
-					<li><a href="./../home/index.php">Home</a></li>
-					<li class='active'><a href="../cadastroCliente/index.php">Cliente</a></li>
-					<li><a href="../cadastroServico/index.php">Serviço</a></li>
-					<li><a href="../cadastroImagen/index.php">Imagens</a></li>
-					<li><a href="../../utils/logout.php">Sair</a></li>
-				</ul>
-			</nav>
-		</header>
+  <div class="splash-screen"></div>
+  <div class="page">
+    <header>
+      <nav>
+        <div class="title-page">
+          <img src="./../../../src/assets/bigode.svg" alt="" />
+        </div>
+        <ul>
+          <li><a href="./../home/index.php">Home</a></li>
+          <li class='active'><a href="../cadastroCliente/index.php">Cliente</a></li>
+          <li><a href="../cadastroServico/index.php">Serviço</a></li>
+          <li><a href="../cadastroImagen/index.php">Imagens</a></li>
+          <li><a href="../../utils/logout.php">Sair</a></li>
+        </ul>
+      </nav>
+    </header>
 
-		<main>
-			<div id="cadastro">
-				<form action="cadastrar-cliente.php" method="POST" id="cadastro_form">
-					<header>
-						<h3>Cadastro!</h3>
-					</header>
+    <main>
+      <div id="cadastro">
+        <form action="cadastrar-cliente.php" method="POST" id="cadastro_form">
+          <header>
+            <h3>Cadastro!</h3>
+          </header>
 
-					<fieldset>
-						<label for="txtNome">Nome: </label>
-						<input type="text" id="txtNomeCliente" name="txtNomeCliente" placeholder="Seu nome" required maxlength="45">
-					</fieldset>
+          <fieldset>
+            <label for="txtNome">Nome: </label>
+            <input type="text" id="txtNomeCliente" name="txtNomeCliente" placeholder="Seu nome" required maxlength="45">
+          </fieldset>
 
-					<fieldset>
-						<label for="txtNumeroContato">CPF: </label>
-						<input type="Numero" id="txtCpfCliente" name="txtCpfCliente" placeholder="Digite seu CPF" required maxlength="14">
-					</fieldset>
+          <fieldset>
+            <label for="txtNumeroContato">CPF: </label>
+            <input type="Numero" id="txtCpfCliente" name="txtCpfCliente" placeholder="Digite seu CPF" required
+              maxlength="14">
+          </fieldset>
 
-					<fieldset>
-						<label for="txtNome">Email: </label>
-						<input type="email" id="txtEmailCliente" name="txtEmailCliente" placeholder="exemplo@gmail.com" required maxlength="40">
-					</fieldset>
+          <fieldset>
+            <label for="txtNome">Email: </label>
+            <input type="email" id="txtEmailCliente" name="txtEmailCliente" placeholder="exemplo@gmail.com" required
+              maxlength="40">
+          </fieldset>
 
-					<fieldset>
-						<label for="txtNumeroContato">Número para contato: </label>
-						<input type="text" id="txtFoneCliente" name="txtFoneCliente" placeholder="Exemplo: (11) 91234-5678" required maxlength="15">
-					</fieldset>
+          <fieldset>
+            <label for="txtNumeroContato">Número para contato: </label>
+            <input type="text" id="txtFoneCliente" name="txtFoneCliente" placeholder="Exemplo: (11) 91234-5678" required
+              maxlength="15">
+          </fieldset>
 
-					<div id="btn">
-						<button type="reset">Cancelar</button>
-						<button id="btn-next" type="submit">Enviar</button>
-					</div>
-				</form>
+          <div id="btn">
+            <button type="reset">Cancelar</button>
+            <button id="btn-next" type="submit">Enviar</button>
+          </div>
+        </form>
 
-			</div>
+      </div>
 
-		</main>
-	</div>
+    </main>
+  </div>
 
-	<script src="../../../src/components/modal/animatedModal.js"></script>
+  <script src="../../../src/components/modal/animatedModal.js"></script>
 
-	<script>
-		// atribuindo o efeito ao modal
-		$("#demo01").animatedModal();
-		// quando o form for enviado
-		$('#cadastro_form').submit((e) => {
+  <script>
+  // atribuindo o efeito ao modal
+  $("#demo01").animatedModal();
+  // quando o form for enviado
+  $('#cadastro_form').submit((e) => {
 
-			const nomeCliente = $('#txtNomeCliente').val();
-			const cpfCliente = $('#txtCpfCliente').val();
-			const emailCliente = $('#txtEmailCliente').val();
-			const telefoneCliente = $('#txtFoneCliente').val();
-			let state = true;
-			const forbiden = '!%/#$&*;';
-			[...forbiden].map(item => {
-				if ([...nomeCliente].indexOf(item) > -1) {
-					state = false;
-					return;
-				}
-				if ([...cpfCliente].indexOf(item) > -1) {
-					state = false;
-					return;
-				}
-				if ([...emailCliente].indexOf(item) > -1) {
-					state = false;
-					return;
-				}
-				if ([...telefoneCliente].indexOf(item) > -1) {
-					state = false;
-					return;
-				}
-			});
+    const nomeCliente = $('#txtNomeCliente').val();
+    const cpfCliente = $('#txtCpfCliente').val();
+    const emailCliente = $('#txtEmailCliente').val();
+    const telefoneCliente = $('#txtFoneCliente').val();
+    let state = true;
+    const forbiden = '!%/#$&*;';
+    [...forbiden].map(item => {
+      if ([...nomeCliente].indexOf(item) > -1) {
+        state = false;
+        return;
+      }
+      if ([...cpfCliente].indexOf(item) > -1) {
+        state = false;
+        return;
+      }
+      if ([...emailCliente].indexOf(item) > -1) {
+        state = false;
+        return;
+      }
+      if ([...telefoneCliente].indexOf(item) > -1) {
+        state = false;
+        return;
+      }
+    });
 
-			if (state) {
-				// ajax
-				// nada, envia, tamo sem ajax
-			} else {
-				// cancela o evento
-				e.preventDefault();
-				$('#modal').empty();
-				$('#modal').append(
-					'<h3>Não utilize caracteres especiais!</h3>'
-				);
-				$('#demo01').click();
+    if (state) {
+      // ajax
+      // nada, envia, tamo sem ajax
+    } else {
+      // cancela o evento
+      e.preventDefault();
+      $('#modal').empty();
+      $('#modal').append(
+        '<h3>Não utilize caracteres especiais!</h3>'
+      );
+      $('#demo01').click();
 
-			}
-		});
-	</script>
+    }
+  });
+  </script>
 
-	<?php
+  <?php
 	if (isset($_GET['cli_cad'])) {
 		if ($_GET['cli_cad']) {
 			echo ('<script>
--- security/adm/home/index.php
+++ security/adm/home/index.php
@@ -5,131 +5,134 @@ include_once('../../utils/sentinel.php');
 <html>
 
 <head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Bigodudu</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bigodudu</title>
 
-	<!-- fonts -->
-	<link href="https://fonts.googleapis.com/css2?family=Ovo&family=Poppins:wght@500&family=Ubuntu&display=swap" rel="stylesheet" />
-	<script src="https://kit.fontawesome.com/326a212606.js" crossorigin="anonymous"></script>
+  <!-- fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Ovo&family=Poppins:wght@500&family=Ubuntu&display=swap"
+    rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/326a212606.js" crossorigin="anonymous"></script>
 
-	<!-- styles -->
-	<link rel="stylesheet" href="../../../src/styles/reset.css" />
-	<link rel="stylesheet" href="../../../src/styles/default.css" />
-	<link rel="stylesheet" href="../../../src/components/carrousel/style.css" />
-	<link rel="stylesheet" href="../../../src/components/modal/animate.min.css" />
-	<link rel="stylesheet" href="../../../src/components/modal/styles.css" />
-	<link rel="stylesheet" href="../../../src/components/menu/style.css" />
-	<link rel="stylesheet" href="src/home-style.css" />
-	<!-- scripts -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- styles -->
+  <link rel="stylesheet" href="../../../src/styles/reset.css" />
+  <link rel="stylesheet" href="../../../src/styles/default.css" />
+  <link rel="stylesheet" href="../../../src/components/carrousel/style.css" />
+  <link rel="stylesheet" href="../../../src/components/modal/animate.min.css" />
+  <link rel="stylesheet" href="../../../src/components/modal/styles.css" />
+  <link rel="stylesheet" href="../../../src/components/menu/style.css" />
+  <link rel="stylesheet" href="src/home-style.css" />
+  <!-- scripts -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 </head>
 
 <body>
-	<div class="splash-screen"></div>
-	<div class="page">
-		<header>
-			<nav>
-				<div class="title-page">
-					<img src="./../../../src/assets/bigode.svg" alt="" />
-				</div>
-				<ul>
-					<li class='active'><a href="#">Home</a></li>
-					<li><a href="../cadastroCliente/index.php">Cliente</a></li>
-					<li><a href="../cadastroServico/index.php">Serviço</a></li>
-					<li><a href="../cadastroImagen/index.php">Imagens</a></li>
-					<li><a href="../../utils/logout.php">Sair</a></li>
-				</ul>
-			</nav>
-		</header>
-		<main>
-			<div class="titulo">
-				Bem vindo(a), Gerencie seu site!
-			</div>
-			<section class="carrousel">
-				<div class="carrousel-container">
-					<div class="btn-container">
-						<label id='btn-prev'><i class="fas fa-arrow-up"></i></label>
-					</div>
-					<div class="itens-container">
-						<div class="item" id="item">
-							<div class="line1">
-								<h3 class="icon-item"><i class="fas fa-cut"></i></h3>
-							</div>
-							<div class="line2">
-								<h4>"Ambiente espetacular, atendimento excelente"</h4>
-							</div>
-						</div>
-						<div class="item" id="item">
-							<div class="line1">
-								<h3 class="icon-item"><i class="fas fa-cut"></i></h3>
-							</div>
-							<div class="line2">
-								<h4>"Ambiente legal, atendimento excelente"</h4>
-							</div>
-						</div>
-						<div class="item" id="item">
-							<div class="line1">
-								<h3 class="icon-item"><i class="fas fa-cut"></i></h3>
-							</div>
-							<div class="line2">
-								<h4>"Ambiente top, atendimento excelente"</h4>
-							</div>
-						</div>
-						<div class="item" id="item">
-							<div class="line1">
-								<h3 class="icon-item"><i class="fas fa-cut"></i></h3>
-							</div>
-							<div class="line2">
-								<h4>"Ambiente bacana, atendimento excelente"</h4>
-							</div>
-						</div>
-					</div>
-					<div class="btn-container">
-						<label id='btn-next'><i class="fas fa-arrow-up"></i></label>
-					</div>
-				</div>
-			</section>
+  <div class="splash-screen"></div>
+  <div class="page">
+    <header>
+      <nav>
+        <div class="title-page">
+          <img src="./../../../src/assets/bigode.svg" alt="" />
+        </div>
+        <ul>
+          <li class='active'><a href="#">Home</a></li>
+          <li><a href="../cadastroServico/index.php">Serviço</a></li>
+          <li><a href="../cadastroImagen/index.php">Imagens</a></li>
+          <li><a href="../../utils/logout.php">Sair</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <div class="titulo">
+        Bem vindo(a), Gerencie seu site!
+      </div>
+      <section class="carrousel">
+        <div class="carrousel-container">
+          <div class="btn-container">
+            <label id='btn-prev'><i class="fas fa-arrow-up"></i></label>
+          </div>
+          <div class="itens-container">
+            <div class="item" id="item">
+              <div class="line1">
+                <h3 class="icon-item"><i class="fas fa-cut"></i></h3>
+              </div>
+              <div class="line2">
+                <h4>"Ambiente espetacular, atendimento excelente"</h4>
+              </div>
+            </div>
+            <div class="item" id="item">
+              <div class="line1">
+                <h3 class="icon-item"><i class="fas fa-cut"></i></h3>
+              </div>
+              <div class="line2">
+                <h4>"Ambiente legal, atendimento excelente"</h4>
+              </div>
+            </div>
+            <div class="item" id="item">
+              <div class="line1">
+                <h3 class="icon-item"><i class="fas fa-cut"></i></h3>
+              </div>
+              <div class="line2">
+                <h4>"Ambiente top, atendimento excelente"</h4>
+              </div>
+            </div>
+            <div class="item" id="item">
+              <div class="line1">
+                <h3 class="icon-item"><i class="fas fa-cut"></i></h3>
+              </div>
+              <div class="line2">
+                <h4>"Ambiente bacana, atendimento excelente"</h4>
+              </div>
+            </div>
+          </div>
+          <div class="btn-container">
+            <label id='btn-next'><i class="fas fa-arrow-up"></i></label>
+          </div>
+        </div>
+      </section>
 
-			<section id="sobreBarbearia" name="sobreBarbearia">
-				<div class="container-row">
-					<div class="item-texto">
-						<div class="titulo">Sobre a Barbearia Ribeiro</div>
-						<div class="texto">
-							A Barbearia Ribeiro é a melhor barbearia da região
-							excelente em executar sua função realizando diversos
-							tipos de cortes deixando sua barba do jeito certo.
-						</div>
-					</div>
-					<div class="item-container">
-						<img src="./../../../src/assets/home-img1.png" alt="Barbearia">
-					</div>
-				</div>
-				<div class="container-row">
-					<div class="item-container">
-						<img src="./../../../src/assets/home-img02.png" alt="Fazendo a barba">
-					</div>
-					<div class="item-texto">
-						<div class="titulo">Sobre a Barbearia Ribeiro</div>
-						<div class="texto">
-							A Barbearia Ribeiro é a melhor barbearia da região
-							excelente em executar sua função realizando diversos
-							tipos de cortes deixando sua barba do jeito certo.
-						</div>
-					</div>
-				</div>
-			</section>
-			<section id="mapa">
-				<h4> Localização de nosso estabelecimento</h4>
-				<div class="map">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2490.957690505681!2d-46.39817244017515!3d-23.60247458322646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1spt-BR!2sbr!4v1598043462204!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-				</div>
-			</section>
-		</main>
-	</div>
-	</div>
-	<script src="../../../src/components/carrousel/main.js"></script>
+      <section id="sobreBarbearia" name="sobreBarbearia">
+        <div class="container-row">
+          <div class="item-texto">
+            <div class="titulo">Sobre a Barbearia Ribeiro</div>
+            <div class="texto">
+              A Barbearia Ribeiro é a melhor barbearia da região
+              excelente em executar sua função realizando diversos
+              tipos de cortes deixando sua barba do jeito certo.
+            </div>
+          </div>
+          <div class="item-container">
+            <img src="./../../../src/assets/home-img1.png" alt="Barbearia">
+          </div>
+        </div>
+        <div class="container-row">
+          <div class="item-container">
+            <img src="./../../../src/assets/home-img02.png" alt="Fazendo a barba">
+          </div>
+          <div class="item-texto">
+            <div class="titulo">Sobre a Barbearia Ribeiro</div>
+            <div class="texto">
+              A Barbearia Ribeiro é a melhor barbearia da região
+              excelente em executar sua função realizando diversos
+              tipos de cortes deixando sua barba do jeito certo.
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="mapa">
+        <h4> Localização de nosso estabelecimento</h4>
+        <div class="map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2490.957690505681!2d-46.39817244017515!3d-23.60247458322646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1spt-BR!2sbr!4v1598043462204!5m2!1spt-BR!2sbr"
+            width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+            tabindex="0"></iframe>
+        </div>
+      </section>
+    </main>
+  </div>
+  </div>
+  <script src="../../../src/components/carrousel/main.js"></script>
 </body>
 
 </html>
