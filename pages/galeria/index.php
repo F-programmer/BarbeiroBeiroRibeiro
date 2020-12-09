5c9e6433a726851b4a3009483e6e7dc69ed4f3ca
Flavio Henrique <flaviovhtorres@gmail.com>
2020-12-09 00:30:13 -0300

    new gallery, fixing responsiveness

--- pages/galeria/index.php
+++ pages/galeria/index.php
@@ -9,85 +9,88 @@ require_once '../../security/utils/autoloaderGaleria.php';
 <html>
 
 <head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Bigodudu</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bigodudu</title>
 
-	<!-- fonts -->
-	<link href="https://fonts.googleapis.com/css2?family=Ovo&family=Poppins:wght@500&family=Ubuntu&display=swap" rel="stylesheet" />
-	<script src="https://kit.fontawesome.com/326a212606.js" crossorigin="anonymous"></script>
+  <!-- fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Ovo&family=Poppins:wght@500&family=Ubuntu&display=swap"
+    rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/326a212606.js" crossorigin="anonymous"></script>
 
-	<!-- styles -->
-	<link rel="stylesheet" href="../../src/styles/reset.css" />
-	<link rel="stylesheet" href="../../src/styles/default.css" />
-	<link rel="stylesheet" href="../../src/components/menu/style.css" />
-	<link rel="stylesheet" href="../../src/components/modal/animate.min.css" />
-	<link rel="stylesheet" href="../../src/components/modal/styles.css" />
+  <!-- styles -->
+  <link rel="stylesheet" href="../../src/styles/reset.css" />
+  <link rel="stylesheet" href="../../src/styles/default.css" />
+  <link rel="stylesheet" href="../../src/components/menu/style.css" />
+  <link rel="stylesheet" href="../../src/components/modal/animate.min.css" />
+  <link rel="stylesheet" href="../../src/components/modal/styles.css" />
 
-	<link rel="stylesheet" href="./src/styles.css" />
+  <link rel="stylesheet" href="./src/styles.css" />
 
-	<!-- scripts -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="../../../src/components/modal/animatedModal.js"></script>
+  <!-- scripts -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="../../../src/components/modal/animatedModal.js"></script>
 
 </head>
 
 <body>
 
-	<header>
-		<nav>
-			<div class="title-page">
-				<img src="../../src/assets/bigode.svg" alt="" />
-			</div>
-			<ul>
-				<li><a href="../../index.php">Home</a></li>
-				<li><a href="../login/login.php">Login</a></li>
-				<li class='active'><a href="../cadastroServico/index.php">Galeria</a></li>
-			</ul>
-		</nav>
-	</header>
+  <header>
+    <nav>
+      <div class="title-page">
+        <img src="../../src/assets/bigode.svg" alt="" />
+      </div>
+      <ul>
+        <li><a href="../../index.php">Home</a></li>
+        <li><a href="../login/login.php">Login</a></li>
+        <li class='active'><a href="../cadastroServico/index.php">Galeria</a></li>
+      </ul>
+    </nav>
+  </header>
 
 
-	<div id="titulo">
-		<h1>Galeria</h1>
-	</div>
+  <div id="titulo">
+    <h1>Galeria</h1>
+  </div>
 
-	<container class="galeria-container">
+  <main>
+    <section>
+      <?php
 
-		<section class="galeria-itens">
-			<?php
 			$imagens = DataProvider::listarImagens();
-			$par = true;
+
 			foreach ($imagens as &$item) {
-				$content = '<div class="item-container "' . (($par) ? 'item-left' : 'item-right') . '>'
+				$html = '<div class="grid-item"><div class="title-card"><span class="title">';
+				$html .= $item['nomeImagem'];
+				$html .= '</span><span class="decription">';
+				$html .= $item['descImagem'];
+				$html .= '</span></div><div class="imagem-card">';
+				$html .= '<img src="../../' . $item['urlImagem'] . "/" . $item['nomeImagem'] . '">';
+				$html .= '<span class="hidden-text">';
+				$html .= $item['descricaoServico'];
+				$html .= '</span></div></div></div>';
 
-					. '<div class="img">'
-
-					. '<img src="../../'
-					. $item['urlImagem'] . '/' . $item['nomeImagem'] . '"'
-					. ' alt ="' . $item['descImagem'] . '"'
-					. ' title ="' . $item['descImagem'] . '"'
-					. ' class="item"'
-					. '>'
-					. '</div>'
-
-					. '<div class="item-titulo">'
-
-					. '<div class="titulo">'
-					. $item['descricaoServico']
-					. '<span>' . $item['descImagem'] . '<span>'
-					. '</div>'
-
-					. '</div>'
-					. '</div>';
-
-				echo $content;
+				echo $html;
 			}
+
 			?>
-		</section>
-
-	</container>
-
+      <div class="grid-item">
+        <div class="title-card">
+          <span class="title">Essa imagem</span>
+          <span class="decription">Essa imagem eh do Leo Valdez, um personagem que eu gosto</span>
+        </div>
+        <div class="imagem-card">
+          <img src="https://pm1.narvii.com/6473/654fa17f4bc1df6824e23f15d472d133bc4c6899_00.jpg">
+          <span class="hidden-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis aliquid quisquam rerum maiores
+            facilis nam, soluta, qui a sed adipisci nulla labore ab eveniet veniam doloremque quibusdam veritatis dicta!
+            Quidem.
+          </span>
+        </div>
+      </div>
+      </div>
+    </section>
+  </main>
 </body>
 
 </html>
--- security/models/Conexao.php
+++ security/models/Conexao.php
@@ -6,13 +6,14 @@ class Conexao
 	{
 		//conexão local
 		$conexao = new PDO(
-		/*	"mysql:host=localhost;dbname=bdbarbearia",
+			/*	"mysql:host=localhost;dbname=bdbarbearia",
 			"root",    fdb30.awardspace.net
 			""*/
-			"mysql:host=fdb30.awardspace.net;
-			dbname=3676945_bdbarbearia",
-			"3676945_bdbarbearia",
-			"3lB1g0d0n"); 
+			"mysql:host=localhost;
+			dbname=bdBarbearia",
+			"root",
+			""
+		);
 
 		$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$conexao->exec("SET CHARACTER SET utf8");
